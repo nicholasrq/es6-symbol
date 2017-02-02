de482cf6ef10650a95f72d1445ecec374969a8d1
Nicholas <nr@fenelon.ru>
2017-02-02 15:36:52 +0300

    fix

--- is-native-implemented.js
+++ is-native-implemented.js
@@ -2,4 +2,4 @@
 
 'use strict';
 
-module.exports = typeof Symbol === 'function' && typeof Symbol() === 'symbol';
+module.exports = typeof Symbol === 'function' && Object.prototype.toString.call(Symbol.prototype) === '[object Symbol]';
